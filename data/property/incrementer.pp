5f460b60e558822bada88adeab83c06101930384
Nathanael Sensfelder <SpamShield0@MultiAgentSystems.org>
2017-08-17 11:50:28 +0200

    "LINE" was in undercase, messing with the output.

--- data/property/incrementer.pp
+++ data/property/incrementer.pp
@@ -1,3 +1,3 @@
 The waveform associated with $wf.IDENTIFIER$ (from file $wf.FILE$, line $wf.LINE$
 column $wf.COLUMN$) is incremented by the process $ps.LABEL$ (from
-$ps.FILE$, line $ps.line$, column $ps.COLUMN$).
+$ps.FILE$, line $ps.LINE$, column $ps.COLUMN$).
